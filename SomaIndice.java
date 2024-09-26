ecc9d63760d400dfe5035abc277b88c9cbef32b3
LilGlen <glendasouzasantos6@gmail.com>
2024-09-26 19:24:12 -0300

    Add files via upload

---
+++ SomaIndice.java
@@ -0,0 +1,15 @@
+public class SomaIndice {
+    public static void main(String[] args) {
+        // Declaração das variáveis
+        int INDICE = 12;
+        int SOMA = 0;
+        int K = 1;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+        System.out.println("O valor da soma é: " + SOMA);
+    }
+}
+
